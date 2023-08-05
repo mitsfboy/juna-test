b47db3732a0a90025b555c02bc4f70a13208cf71
nachodev-ui <ig.cisternas@duocuc.cl>
2023-08-05 14:18:08 -0400

    feat: Promotions component is rendering a Carousel by material-tailwind (in development)

---
+++ components/Promotions.tsx
@@ -0,0 +1,8 @@
+import { Carousel } from '@material-tailwind/react'
+import Image from 'next/image'
+
+const Promotions = () => {
+  return <div className="w-full"></div>
+}
+
+export default Promotions
