33b2e6ccb05f6d06f0152928057059fc3a3570ad
nachodev-ui <ig.cisternas@duocuc.cl>
2023-08-06 13:31:17 -0400

    feat: Type for store data extracted by the API

---
+++ types/Store.ts
@@ -0,0 +1,14 @@
+export type Store = {
+  id: number
+  nombre: string
+  direccion: string
+  ciudad: string
+  comuna: string
+  redes: string
+  especialidad: string
+  descripcion: string
+  logo: string
+  imagen_tienda: string
+  url: string
+}
+
