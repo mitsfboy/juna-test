be102888171d2d3a63dc336d4e83669a16cc6d1d
nachodev-ui <ig.cisternas@duocuc.cl>
2023-08-06 13:15:55 -0400

    feat: Deleted Link elements from Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,26 +1,15 @@
 import Image from 'next/image'
-import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between p-4">
+    <nav className="flex items-center justify-center py-14">
       <div className="flex items-center">
         <Image
           src="https://res.cloudinary.com/dj4pfykc3/image/upload/v1691205071/zyro-image-PhotoRoom.png-PhotoRoom_tdtlhb.png"
           alt="Logo"
-          className="w-240 h-240"
-          width={140}
-          height={140}
+          width={240}
+          height={200}
         />
-      </div>
-
-      <div className="flex space-x-4">
-        <Link href="/" className="text-[#663300]">
-          Inicio
-        </Link>
-        <Link href="/about" className="text-[#663300]">
-          Acerca
-        </Link>
       </div>
     </nav>
   )
