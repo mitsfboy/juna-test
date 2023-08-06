c136666decd2ba2e1373274ca0e7f8bd80655c0b
nachodev-ui <ig.cisternas@duocuc.cl>
2023-08-05 20:02:20 -0400

    feat: Functional filters by Speciality - using useStores hook

---
+++ components/Filters.tsx
@@ -0,0 +1,34 @@
+import { Fragment, useState } from 'react'
+import { filterOptions } from '@/mocks/filterOptions'
+import { useStores } from '@/hooks/useStore'
+
+const Filters = () => {
+  const { obtenerEspecialidad } = useStores()
+  const [active, setActive] = useState('todos')
+
+  const handleButtonClick = (value: string) => {
+    obtenerEspecialidad(value)
+    setActive(value)
+  }
+
+  return (
+    <div className="flex flex-wrap justify-center items-center gap-6 md:gap-14 my-20 text-[#663300]">
+      {filterOptions.map(option => (
+        <Fragment key={option.value === 'todos' ? 'todos' : option.value}>
+          <button
+            key={option.value}
+            onClick={() => handleButtonClick(option.value)}
+            className={`flex-col items-center justify-center w-40 text-center rounded-lg flex space-y-1 bg-[#f8f8f8] shadow-md hover:bg-[#663300] hover:text-white transition-all mt-4 md:10 p-3 ${
+              active === option.value ? 'bg-[#663300] text-white' : ''
+            }`}
+          >
+            <div className="mb-2 p-2">{option.icon}</div>
+            <div className="font-semibold text-sm">{option.label}</div>
+          </button>
+        </Fragment>
+      ))}
+    </div>
+  )
+}
+
+export default Filters
