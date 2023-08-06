d5a574ac4340e3b4a1d9ab63ecce704248be9c4c
nachodev-ui <ig.cisternas@duocuc.cl>
2023-08-06 13:40:13 -0400

    feat: Config adapted for use material tailwind dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require('@material-tailwind/react/utils/withMT')
+
+module.exports = withMT({
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './layouts/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  corePlugins: {
+    layers: ['utilities'],
+  },
   theme: {
     extend: {
       backgroundImage: {
@@ -15,4 +21,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+})
