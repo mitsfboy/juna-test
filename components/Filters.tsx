527a6aa8cd70fe0d9e8346ae68a1eee4bb2f2763
nachodev-ui <ig.cisternas@duocuc.cl>
2023-08-06 13:14:19 -0400

    feat: Styles from Filters.tsx have been changed

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,10 +1,10 @@
-import { Fragment, useState } from 'react'
-import { filterOptions } from '@/mocks/filterOptions'
-import { useStores } from '@/hooks/useStore'
+import { Fragment, useState } from "react"
+import { filterOptions } from "@/mocks/filterOptions"
+import { useStores } from "@/hooks/useStore"
 
 const Filters = () => {
   const { obtenerEspecialidad } = useStores()
-  const [active, setActive] = useState('todos')
+  const [active, setActive] = useState("todos")
 
   const handleButtonClick = (value: string) => {
     obtenerEspecialidad(value)
@@ -12,14 +12,14 @@ const Filters = () => {
   }
 
   return (
-    <div className="flex flex-wrap justify-center items-center gap-6 md:gap-14 my-20 text-[#663300]">
-      {filterOptions.map(option => (
-        <Fragment key={option.value === 'todos' ? 'todos' : option.value}>
+    <div className="flex flex-wrap justify-center items-center gap-6 md:gap-14 my-20 text-brown-700">
+      {filterOptions.map((option) => (
+        <Fragment key={option.value === "todos" ? "todos" : option.value}>
           <button
             key={option.value}
             onClick={() => handleButtonClick(option.value)}
-            className={`flex-col items-center justify-center w-40 text-center rounded-lg flex space-y-1 bg-[#f8f8f8] shadow-md hover:bg-[#663300] hover:text-white transition-all mt-4 md:10 p-3 ${
-              active === option.value ? 'bg-[#663300] text-white' : ''
+            className={`flex-col items-center justify-center w-40 text-center rounded-lg flex space-y-1 bg-[#f8f8f8] shadow-md hover:bg-brown-600 hover:text-white transition-all mt-4 md:10 p-3 ${
+              active === option.value ? "bg-brown-600 text-white" : ""
             }`}
           >
             <div className="mb-2 p-2">{option.icon}</div>
